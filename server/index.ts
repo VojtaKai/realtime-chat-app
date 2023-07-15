3611564124bb17f89a37d3d89bb7f233fd1f55f5
Vojtech Kaiser <vksmail95@gmail.com>
2023-07-15 17:05:27 +0200

    express router to express app

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as socketio from 'socket.io'
 import * as http from 'http'
+import { router } from './router'
 
 const PORT = Number(process.env.PORT) || 3000
 
@@ -16,6 +17,6 @@ io.on('connnection', (socket) => {
     console.log('connected', socket)
 })
 
-console.log('here oustide')
+app.use(router)
 
 server.listen(PORT, () => (console.log(`Server is running on port ${PORT}`)))
